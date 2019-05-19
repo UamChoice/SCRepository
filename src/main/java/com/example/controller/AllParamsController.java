3bfa2c7474f25a8abdd21ff7b7837d957f159fea
UamChoice <523649919@qq.com>
2019-05-19 18:33:12 +0800

    logback日志结束

--- src/main/java/com/example/controller/AllParamsController.java
+++ src/main/java/com/example/controller/AllParamsController.java
@@ -32,8 +32,12 @@ import io.swagger.annotations.ApiParam;
 @RequestMapping("/receiveParams")
 @Api(tags="接收参数",description="废弃的")
 public class AllParamsController {
-	
-	private Logger logger = LoggerFactory.getLogger(AllParamsController.class);
+	/**
+	 * 1.定义成static final,logger变量不可变，读取速度快
+	   2.static 修饰的变量是不管创建了new了多少个实例，也只创建一次，节省空间，如果每次都创建Logger的话比较浪费内存；final修饰表示不可更改，常量
+	   3.将域定义为static,每个类中只有一个这样的域.而每一个对象对于所有的实例域却都有自己的一份拷贝.，用static修饰既节约空间，效率也好。final 是本 logger 不能再指向其他 Logger 对象
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(AllParamsController.class);
 	
 	//相当于@RequestMapping(method = RequestMethod.GET/POST)
 	@GetMapping("/ptPam")
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+	private static final Logger DemoTestLogger = LoggerFactory.getLogger(DemoApplicationTests.class);
 	@Test
 	public void contextLoads() {
+		testErrLog();
 	}
-
+	
+	private static void testErrLog() {
+		try {
+			int a = 5/0;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			DemoTestLogger.error(e.toString());
+			e.printStackTrace();
+		}
+	}
 }
